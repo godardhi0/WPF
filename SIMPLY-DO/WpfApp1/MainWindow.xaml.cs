6c37bb6f5bdf0e4562bc9eb5b5c200fdf8593385
godardhi0 <ishimwegodardquanta66@gmail.com>
2025-10-09 14:54:20 +0200

    added SIMPLY-DO APP

---
+++ SIMPLY-DO/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int taskCount = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonAdd_click(object sender, RoutedEventArgs e)
+        {
+            
+            if(!string.IsNullOrEmpty(txtTasks.Text) && !listTasks.Items.Contains(txtTasks))
+            {
+                taskCount++;
+                listTasks.Items.Add(taskCount.ToString() + ": " + txtTasks.Text);
+                txtTasks.Clear();
+            }
+        }
+
+        private void buttonClear_click(object sender, RoutedEventArgs e)
+        {
+            if(listTasks.Items.Count > 0)
+            {
+                listTasks.Items.Clear();
+                taskCount = 0; 
+            }
+        }
+    }
+}
